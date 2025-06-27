6b82dbde9f96795427afb7fc4abe47d46b300ddb
BiancaDSantos <biancadossantossc@gmail.com>
2025-06-27 16:45:51 -0300

    Requisição dos dados do projeto do tipo get.

--- FinanCiencia/src/services/projectRequests.js
+++ FinanCiencia/src/services/projectRequests.js
@@ -1,1 +1,11 @@
-//  procurar, buscar um
+//  procurar, buscar um, SÓ TIPO GET
+
+import api from './api';
+
+const projectService = {
+  listProjects: () => api.get('/projeto/listar'),
+  searchProjects: (title) => api.get('/projeto/buscar', { params: { tituloProjeto: title } }),
+  getProjectDetails: (id) => api.get(`/projeto/visualizar/${id}`),
+};
+
+export default projectService;
