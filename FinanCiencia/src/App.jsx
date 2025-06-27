17bad94b809a8d29830abc6a04d8b1472909f27b
BiancaDSantos <biancadossantossc@gmail.com>
2025-06-27 17:20:07 -0300

    Limpeza, dados inuteis.

--- FinanCiencia/src/App.jsx
+++ FinanCiencia/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import MainLayout from './layouts/MainLayout'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <MainLayout></MainLayout>
   )
